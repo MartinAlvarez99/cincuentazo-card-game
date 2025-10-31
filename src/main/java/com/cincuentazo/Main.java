b8a18dda40c279e7839d5c01a66679c0ecce3bfd
Howard <howard.sanchez@correounivalle.edu.co>
2025-10-31 18:21:11 -0500

    Creación del tablero y las cartas

--- src/main/java/com/cincuentazo/Main.java
+++ src/main/java/com/cincuentazo/Main.java
@@ -10,11 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Load player selection view (HU-1)
-        Parent root = FXMLLoader.load(getClass().getResource("/com/cincuentazo/view/PlayerSelectionView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/cincuentazo/view/Game.fxml"));
         primaryStage.setTitle("Cincuentazo - Card Game");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.setResizable(false); // Ventana fija, no redimensionable
         primaryStage.show();
     }
 
